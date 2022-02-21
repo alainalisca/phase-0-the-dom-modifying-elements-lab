69aea5f72008f547cc4b40b265f852a3093f39b4
Alain Alisca <alainalisca@gmail.com>
2022-02-20 20:26:07 -0500

    commit tree 8980c7cc5a3064e301ab5542cdf61d4e1cd597a8

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here!
+
+document.querySelector("main");
+
+main.remove(main);
+
+// newHeader.Victory === H1; 
+
+// newHeader.id 
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+
+newHeader.innerHTML = "Alisca is the champion";
+
+
+
+
+
+// expect(newHeader.nodeName, create an <h1> with id 'victory'").eql('H1')
+
+// expect(newHeader.id, create an <h1> with id 'victory'").eql('victory')
+
+// expect(newHeader.innerHTML, create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
